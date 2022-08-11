63027500828f25594c475f87a69c8dc8bdb74996
nachowolf <ignaciowolf10@gmail.com>
2022-08-10 21:06:21 -0300

    Eliminar productos desde el Front

--- public/inedx.js
+++ public/inedx.js
@@ -1,7 +1,9 @@
+const prodContainer = document.getElementById('prodContainer')
+
+//Renderiza un producto al html
 function renderNewProduct(product) {
-    const prodContainer = document.getElementById('prodContainer')
     prodContainer.innerHTML += `
-        <li class="col-12">
+        <li class="col-12" id="${product.id}">
             <div class="row g-0">
                 <div class="col-6"><img src="${product.url}" alt=""></div>
                 <div class="col-6 d-flex flex-column">
@@ -13,7 +15,7 @@ function renderNewProduct(product) {
                     <div class="d-flex flex-row justify-content-between text-sm">Stock: ${product.stock}</div>
                     <div class="align-self-end mt-3">
                         <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-pencil-square"></i></button>
-                        <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
+                        <button class="btn btn-danger bi bi-trash deleteProd"></button>
                     </div>
                 </div>
             </div>
@@ -21,6 +23,7 @@ function renderNewProduct(product) {
     `
 }
 
+//Renderiza muchos productos
 function renderProducts(data) {
     if (data.length > 0) {
         data.forEach(product => {
@@ -29,6 +32,7 @@ function renderProducts(data) {
     }
 }
 
+//Trae los productos de la base
 function loadProducts() {
     fetch('/api/productos')
         .then(data => data.json())
@@ -36,3 +40,12 @@ function loadProducts() {
 }
 
 loadProducts();
+
+//Funcion para que se elimine el producto segun el boton que toques.
+prodContainer.addEventListener('click',(e)=>{
+    if(e.target.className.includes('deleteProd')){
+        const idProd = e.target.parentNode.parentNode.parentNode.parentNode.id
+        e.target.parentNode.parentNode.parentNode.parentNode.remove()
+        fetch(`api/productos/${idProd}`,{method:'DELETE'})
+    }
+})
